6a33d0bee7ae857f9a92d29bb60171bd1c27be0b
angelOrtega <arortega47@misena.edu.co>
2023-06-11 02:37:32 -0500

    Modificacion archivo readme

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de empleados</title>
+    <title>Gestión de empleados</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
